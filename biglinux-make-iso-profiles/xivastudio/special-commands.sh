9c2b0714e9bec9e481199933ab09b9be107f5a43
gitbarnabedikartola <110747604+gitbarnabedikartola@users.noreply.github.com>
2023-04-11 12:50:03 -0300

    fix special command for xivastudio

--- biglinux-make-iso-profiles/xivastudio/special-commands.sh
+++ biglinux-make-iso-profiles/xivastudio/special-commands.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
     
 # Add some things from Packages-Desktop
-sed -n '/## Printing/,/^$/p'  manjaro-iso-profiles/manjaro/kde/Packages-Desktop  >>  biglinux/kde/Packages-Desktop
-sed -n '/## Xorg Server and Graphics/,/^$/p'  manjaro-iso-profiles/manjaro/kde/Packages-Desktop  >>  biglinux/kde/Packages-Desktop
-sed -n '/## Xorg Input Drivers/,/^$/p'  manjaro-iso-profiles/manjaro/kde/Packages-Desktop  >>  biglinux/kde/Packages-Desktop
-sed -n '/## Misc/,/^$/p'  manjaro-iso-profiles/manjaro/kde/Packages-Desktop  >>  biglinux/kde/Packages-Desktop
+sed -n '/## Printing/,/^$/p'  manjaro-iso-profiles/manjaro/kde/Packages-Desktop  >>  biglinux/xivastudio/Packages-Desktop
+sed -n '/## Xorg Server and Graphics/,/^$/p'  manjaro-iso-profiles/manjaro/kde/Packages-Desktop  >>  biglinux/xivastudio/Packages-Desktop
+sed -n '/## Xorg Input Drivers/,/^$/p'  manjaro-iso-profiles/manjaro/kde/Packages-Desktop  >>  biglinux/xivastudio/Packages-Desktop
+sed -n '/## Misc/,/^$/p'  manjaro-iso-profiles/manjaro/kde/Packages-Desktop  >>  biglinux/xivastudio/Packages-Desktop
 sed -i 's|xf86-input-void||g'  biglinux/kde/Packages-Desktop
+
+sed -i '/GRUB_DISTRIBUTOR=/s/GRUB_DISTRIBUTOR=.*/GRUB_DISTRIBUTOR="XIVAStudio"/' root-overlay/etc/default/grub
