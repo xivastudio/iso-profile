a917f13ec396708f6330ffcc75631e9943312d6c
gitbarnabedikartola <110747604+gitbarnabedikartola@users.noreply.github.com>
2023-04-11 13:08:05 -0300

    new seds

--- biglinux-make-iso-profiles/xivastudio/special-commands.sh
+++ biglinux-make-iso-profiles/xivastudio/special-commands.sh
@@ -8,4 +8,23 @@ sed -n '/## Misc/,/^$/p'  manjaro-iso-profiles/manjaro/kde/Packages-Desktop  >> 
 sed -i 's|xf86-input-void||g'  biglinux/kde/Packages-Desktop
 
 sed -i '/GRUB_DISTRIBUTOR=/s/GRUB_DISTRIBUTOR=.*/GRUB_DISTRIBUTOR="XIVAStudio"/' biglinux/xivastudio/root-overlay/etc/default/grub
-# sed -i '//s//' biglinux/xivastudio/
+sed -i '/User=/s/User=.*/User=xivastudio/' biglinux/xivastudio/root-overlay/etc/sddm.conf
+sed -i '/User=/s/User=.*/User=xivastudio/' biglinux/xivastudio/live-overlay/etc/sddm.conf
+sed -i '/autologin=/s/autologin=.*/autologin=xivastudio/' biglinux/xivastudio/live-overlay/etc/lxdm/lxdm.conf
+sed -i '/Current=/s/Current=.*/Current=xivastudio/' biglinux/xivastudio/live-overlay/etc/sddm.conf
+sed -i '/misolabel=/s/misolabel=.*/misolabel=xivastudio/g' biglinux/xivastudio/live-overlay/usr/share/grub/cfg/kernels.cfg
+sed -i '/misolabel=/s/misolabel=.*/misolabel=xivastudio/g' biglinux/xivastudio/live-overlay/boot/grub/kernels.cfg
+sed -i '/file = /s/file = .*/file = "xivastudio.png"/' biglinux/xivastudio/live-overlay/usr/share/grub/themes/manjaro-live/theme.txt
+sed -i '/file = /s/file = .*/file = "xivastudio.png"/' biglinux/xivastudio/live-overlay/usr/share/grub/themes/biglinux-live/theme.txt
+sed -i '/hostname=/s/hostname=.*/hostname="xivastudio"/' biglinux/xivastudio/profile.conf
+sed -i '/username=/s/username=.*/username="xivastudio"/' biglinux/xivastudio/profile.conf
+sed -i '/password=/s/password=.*/password="xivastudio"/' biglinux/xivastudio/profile.conf
+
+
+# sed -i '//s///' biglinux/xivastudio/
+# sed -i '//s///' biglinux/xivastudio/
+# sed -i '//s///' biglinux/xivastudio/
+# sed -i '//s///' biglinux/xivastudio/
+# sed -i '//s///' biglinux/xivastudio/
+# sed -i '//s///' biglinux/xivastudio/
+# sed -i '//s///' biglinux/xivastudio/
